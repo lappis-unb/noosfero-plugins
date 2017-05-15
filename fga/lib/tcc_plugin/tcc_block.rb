545938033938dcc47fd529e66138cd36eecb5549
LucasMartins <lucas465@hotmail.com>
2017-05-15 13:33:35 -0300

    Initial commit FGA plugins

---
+++ fga/lib/tcc_plugin/tcc_block.rb
@@ -0,0 +1,14 @@
+class TccBlock < Block
+
+  def self.description
+    _('TCC')
+  end
+
+  def help
+    _('This block displays a link to TCC\'s help page.')
+  end
+
+  def view_title
+    _('Submit TCC')
+  end
+end
